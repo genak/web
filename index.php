2a8159907798f1aa0502d242b0655d75cda3d3f1
fenix0x <genk@front.ru>
2015-04-27 19:29:41 +0300

    add

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php
     require_once('/include/common.inc.php');
+    $topmenus = array(
+            array('class' => 'show', 'icon_class' => 'icon-main', 'text' => 'Основное'),
+            array('class' => 'hide', 'icon_class' => 'icon-html', 'text' => 'HTML'),
+            array('class' => 'hide', 'icon_class' => 'icon-css', 'text' => 'CSS'),
+            array('class' => 'hide', 'icon_class' => 'icon-site', 'text' => 'Сайт')
+        );
+    $submenus = array(
+            array('href' => '/content', 'text' => 'Статьи'),
+            array('class' => '/blog', 'text' => 'Блог'),
+            array('class' => '/practical', 'text' => 'Практикум'),
+            array('class' => '/test', 'text' => 'Тесты'),
+            array('class' => 'http://htmlforum.ru', 'text' => 'Форум')
+        );
     $students = array(
             array('name' => 'Alex', 'age' => 20),
             array('name' => 'Tanya', 'age' => 21)
         );
     $vars = array(
-            'students' => $students
+            'students' => $students,
+            'topmenus' => $topmenus,
+            'submenus' => $submenus
         );
     echo BuildPage('students.tpl', $vars);
--- template/header.tpl
+++ template/header.tpl
@@ -1,9 +1,31 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="generator"
+    content="HTML Tidy for HTML5 (experimental) for Windows https://github.com/w3c/tidy-html5/tree/c63cc39" />
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <title></title>
   </head>
   <body>
     <div class="main">
       <div class="header">
+        <img src="../img/logo.png" alt="htmlbook.ru" class="logo" />
+        <nav>
+          <ul id="topmenu">
+            {foreach from=$topmenus item=topmenu}
+              <li class={$topmenu.class|escape:html}><i class={$topmenu.icon_class}></i><br>{$topmenu.text|escape:html}</li>
+            {/foreach}
+          </ul>
+          <div id="main" class="tile show">
+            <ul>
+              {foreach from=$submenus item=submenu}
+              <li>
+                <a href="{$submenu.href|escape:html}">{$submenu.text|escape:html}</a>
+              </li>
+              {/foreach}
+            </ul>
+          </div>
+        </nav>
       </div>
       <div class="content">
